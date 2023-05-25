cd7b2904777dc1b07c44e315c25b3b30e29c41ca
wilsone24 <wilsone@uninorte.edu.co>
2023-05-25 13:29:31 -0500

    intervalos de ocupacion

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,99 +1,110 @@
 let products = {
-    data: [
-      {
-        productName: "Biblioteca Karl C. Parrish Jr.",
-        category: "Topwear",
-        ocuppation: "35",
-        image: "white-tshirt.jpg",
-      },
-      {
-        productName: "Casa de Estudio Alfredo Correa De Andreis",
-        category: "Bottomwear",
-        ocuppation: "64",
-        image: "short-skirt.jpg",
-      },
-      {
-        productName: "Bloque k salon cree",
-        category: "Watch",
-        ocuppation: "99",
-        image: "sporty-smartwatch.jpg",
-      },
-      {
-        productName: "Bloque J sala konder",
-        category: "Jacket",
-        ocuppation: "29",
-        image: "knitted-top.jpg",
-      },
-      {
-        productName: "Bloque b piso 2",
-        category: "Bottomwear",
-        ocuppation: "129",
-        image: "black-leather-jacket.jpg",
-      },
-      {
-        productName: "Bloque F piso 2",
-        category: "Jacket",
-        ocuppation: "89",
-        image: "pink-trousers.jpg",
-      },
-    ],
-  };
-  function createCard(product) {
-    let card = document.createElement("div");
-    card.classList.add("card", product.category);
+  data: [
+    {
+      productName: "Biblioteca Karl C. Parrish Jr.",
+      category: "Topwear",
+      ocuppation: "35",
+      image: "white-tshirt.jpg",
+    },
+    {
+      productName: "Casa de Estudio Alfredo Correa De Andreis",
+      category: "Bottomwear",
+      ocuppation: "64",
+      image: "short-skirt.jpg",
+    },
+    {
+      productName: "Bloque k salon cree",
+      category: "Watch",
+      ocuppation: "99",
+      image: "sporty-smartwatch.jpg",
+    },
+    {
+      productName: "Bloque J sala konder",
+      category: "Jacket",
+      ocuppation: "29",
+      image: "knitted-top.jpg",
+    },
+    {
+      productName: "Bloque b piso 2",
+      category: "Bottomwear",
+      ocuppation: "129",
+      image: "black-leather-jacket.jpg",
+    },
+    {
+      productName: "Bloque F piso 2",
+      category: "Jacket",
+      ocuppation: "89",
+      image: "pink-trousers.jpg",
+    },
+  ],
+};
+function createCard(product) {
+  let card = document.createElement("div");
+  card.classList.add("card", product.category);
 
-    let imgContainer = document.createElement("div");
-    imgContainer.classList.add("image-container");
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("image-container");
 
-    let image = document.createElement("img");
-    image.setAttribute("src", product.image);
-    imgContainer.appendChild(image);
-    card.appendChild(imgContainer);
+  let image = document.createElement("img");
+  image.setAttribute("src", product.image);
+  imgContainer.appendChild(image);
+  card.appendChild(imgContainer);
 
-    let container = document.createElement("div");
-    container.classList.add("container");
+  let container = document.createElement("div");
+  container.classList.add("container");
 
-    let name = document.createElement("h5");
-    name.classList.add("product-name");
-    name.innerText = product.productName.toUpperCase();
-    container.appendChild(name);
+  let name = document.createElement("h5");
+  name.classList.add("product-name");
+  name.innerText = product.productName.toUpperCase();
+  container.appendChild(name);
 
-    let ocuppation = document.createElement("h6");
-    ocuppation.innerText = "Ocupación: " +  product.ocuppation  + " Personas";
-    container.appendChild(ocuppation);
+  let ocuppation = document.createElement("h6");
+  ocuppation.innerText = "Ocupación: " + product.ocuppation + " Personas";
+  container.appendChild(ocuppation);
 
-    card.appendChild(container);
-    return card;
+  card.appendChild(container);
+  return card;
+}
+
+function displayAllProducts() {
+  let productsContainer = document.getElementById("products");
+  productsContainer.innerHTML = "";
+
+  for (let product of products.data) {
+    let card = createCard(product);
+    productsContainer.appendChild(card);
   }
+}
 
-  function displayAllProducts() {
-    let productsContainer = document.getElementById("products");
-    productsContainer.innerHTML = "";
+function updateOccupation() {
+  let cards = document.querySelectorAll(".card");
 
-    for (let product of products.data) {
-      let card = createCard(product);
-      productsContainer.appendChild(card);
-    }
-  }
-
-  function filterProducts(searchInput) {
-    let elements = document.querySelectorAll(".product-name");
-    let cards = document.querySelectorAll(".card");
-
-    elements.forEach((element, index) => {
-      if (element.innerText.includes(searchInput.toUpperCase())) {
-        cards[index].classList.remove("hide");
-      } else {
-        cards[index].classList.add("hide");
-      }
-    });
-  }
-
-  document.getElementById("search-input").addEventListener("input", () => {
-    let searchInput = document.getElementById("search-input").value;
-    filterProducts(searchInput);
+  cards.forEach((card) => {
+    let ocuppationElement = card.querySelector("h6");
+    let randomOccupation = Math.floor(Math.random() * 151); // Genera un número aleatorio entre 0 y 150
+    ocuppationElement.innerText = "Ocupación: " + randomOccupation + " Personas";
   });
+}
 
-  window.onload = () => {
-    displayAllProducts();
-  };
+function filterProducts(searchInput) {
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  elements.forEach((element, index) => {
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      cards[index].classList.remove("hide");
+    } else {
+      cards[index].classList.add("hide");
+    }
+  });
+}
+
+document.getElementById("search-input").addEventListener("input", () => {
+  let searchInput = document.getElementById("search-input").value;
+  filterProducts(searchInput);
+});
+
+window.onload = () => {
+  displayAllProducts();
+  setInterval(updateOccupation, 10000); // Actualiza la ocupación cada 10 segundos
+};
